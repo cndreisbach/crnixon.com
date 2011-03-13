26714d6ffa04c821bc476158cde428bb0cb0970c
Clinton R. Nixon <crnixon@gmail.com>
2011-03-12 21:40:07 -0500

    Added app.rb

---
+++ app.rb
@@ -0,0 +1,72 @@
+module Nesta
+  class FileModel
+    def parse_file
+      first_para, remaining = File.open(@filename).read.split(/\r?\n\r?\n/, 2)
+      @metadata = {}
+      if paragraph_is_metadata(first_para)
+        @markup = remaining
+        data = YAML::load(first_para)
+        if data.is_a?(Hash)
+          data.each do |key, value|
+            @metadata[key.downcase] = value
+          end
+        end
+      else
+        @markup = [first_para, remaining].join("\n\n")
+      end
+    rescue Errno::ENOENT  # file not found
+      raise Sinatra::NotFound
+    end
+  end
+
+  class App < Sinatra::Base
+    helpers do
+      # add helpers
+    end
+
+    get '/robots.txt' do
+      content_type 'text/plain', :charset => 'utf-8'
+      <<-EOF
+# robots.txt
+# See http://en.wikipedia.org/wiki/Robots_exclusion_standard
+      EOF
+    end
+
+    get '/css/:sheet.css' do
+      content_type 'text/css', :charset => 'utf-8'
+      cache sass(params[:sheet].to_sym)
+    end
+
+    get %r{/attachments/([\w/.-]+)} do
+      file = File.join(Nesta::Config.attachment_path, params[:captures].first)
+      send_file(file, :disposition => nil)
+    end
+
+    get '/articles.xml' do
+      content_type :xml, :charset => 'utf-8'
+      set_from_config(:title, :subtitle, :author)
+      @articles = Page.find_articles.select { |a| a.date }[0..9]
+      cache haml(:atom, :format => :xhtml, :layout => false)
+    end
+
+    get '/sitemap.xml' do
+      content_type :xml, :charset => 'utf-8'
+      @pages = Page.find_all
+      @last = @pages.map { |page| page.last_modified }.inject do |latest, page|
+        (page > latest) ? page : latest
+      end
+      cache haml(:sitemap, :format => :xhtml, :layout => false)
+    end
+
+    get '*' do
+      set_common_variables
+      @heading = @title
+      parts = params[:splat].map { |p| p.sub(/\/$/, '') }
+      @page = Nesta::Page.find_by_path(File.join(parts))
+      raise Sinatra::NotFound if @page.nil?
+      @title = @page.title
+      set_from_page(:description, :keywords)
+      cache haml(@page.template, :layout => @page.layout)
+    end
+  end
+end
